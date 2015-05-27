316944675336f5e76ddde982c97adcaab4407cc1
TristanConant <tristanconant@Tristans-MacBook.local>
2015-05-27 16:41:08 -0700

    Sourcing images from json rather than local folder.

--- vote.js
+++ vote.js
@@ -1,47 +1,53 @@
-function Cat(catNum, imgSrc, wins, losses){
-  this.imgSrc = imgSrc;
-  this.wins = wins;
-  this.losses = losses;
-}
-var catArray = [];
+var images = [];
 
-for (var i = 1; i <= 14; i++) {
-  catArray.push(new Cat([i], 'kittenPics/cat'+[i]+'.jpg', 0, 0))
-};
+var pageSetup = function(){
 
-do {
-  var num1 = Math.floor(Math.random() * catArray.length);
-  var num2 = Math.floor(Math.random() * catArray.length);
-} while(num1 === num2);
+  var num1, num2;
 
-function altNum(){
+  console.log(images);
+
+
+  function altNum(num1, num2){
+    var newNum;
+    do {
+     newNum = Math.floor(Math.random() * images.length);
+    } while(newNum === num1 || newNum === num2);
+    return newNum;
+  };
+
+  num1 = Math.floor(Math.random() * images.length);
   do {
-   var newNum = Math.floor(Math.random() * catArray.length);
-   return newNum
-  } while((newNum === num1) && (newNum === num2));
+    num2 = Math.floor(Math.random() * images.length);
+  } while(num1 === num2);
+
+  $(function(){
+    $('#catFight1').attr('src', images[num1].link);
+    $('#catFight2').attr('src', images[num2].link);
+
+    $('#catFight1').click(function(){
+      images.vote += 1;
+      $('#catFight2').attr('src', images[altNum()].imgSrc);
+    });
+
+    $('#catFight2').click(function(){
+      images.vote += 1;
+      $('#catFight1').attr('src', images[altNum()].imgSrc);
+    });
+  });
 };
 
-$(function(){
-  $('#catFight1').attr('src', catArray[num1].imgSrc);
-  $('#catFight2').attr('src', catArray[num2].imgSrc);
-  var wins = catArray[num1].wins;
-
-  $('#catFight1').click(function(){
-    catArray[num1].wins += 1;
-    catArray[num2].losses += 1;
-    console.log(catArray[num1].wins);
-    console.log(catArray[num1].losses);
-    console.log(catArray[num2].wins);
-    console.log(catArray[num2].losses);
-    $('#catFight2').attr('src', catArray[altNum()].imgSrc);
+ $.ajax({
+   url: "https://api.imgur.com/3/album/FLB8V/images",
+   headers:{
+    "Authorization": "Client-ID 52a6434c730ea88"
+    }
+  })
+ .done(function(res) {
+    console.log(res.data[1]);
+    images = res.data;
+    pageSetup();
   });
 
-  $('#catFight2').click(function(){
-    catArray[num2].wins += 1;
-    catArray[num1].losses += 1;
-    $('#catFight1').attr('src', catArray[altNum()].imgSrc);
-  });
-});
 
 
 
