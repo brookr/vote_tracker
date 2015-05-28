f35a47eaf18564fde754f7c8c9c6a2e4ecf1f0fc
Brook Riggio <brook@rigg.io>
2015-05-28 10:12:06 -0700

    Refactor to use functional programming
    Moved scripts to top.
    Added pageSetup in  to avoid race condition.

--- vote.js
+++ vote.js
@@ -1,54 +1,45 @@
-var images = [];
 
-var pageSetup = function(){
 
+function altNum(num1, num2, max) {
+  var newNum;
+  do {
+   newNum = Math.floor(Math.random() * max);
+  } while(newNum === num1 || newNum === num2);
+  return newNum;
+}
+
+var pageSetup = function(images) {
   var num1, num2;
 
   console.log(images);
 
+  num1 = altNum(num1, num2, images.length);
+  num2 = altNum(num1, num2, images.length);
 
-  function altNum(num1, num2){
-    var newNum;
-    do {
-     newNum = Math.floor(Math.random() * images.length);
-    } while(newNum === num1 || newNum === num2);
-    return newNum;
-  };
-
-  num1 = Math.floor(Math.random() * images.length);
-  do {
-    num2 = Math.floor(Math.random() * images.length);
-  } while(num1 === num2);
-
-  $(function(){
-    $('#catFight1').attr('src', images[num1].link);
-    $('#catFight2').attr('src', images[num2].link);
-
-    $('#catFight1').click(function(){
-      images.vote += 1;
-      $('#catFight2').attr('src', images[altNum()].imgSrc);
-    });
-
-    $('#catFight2').click(function(){
-      images.vote += 1;
-      $('#catFight1').attr('src', images[altNum()].imgSrc);
-    });
-  });
+  $('#catFight1').attr('src', images[num1].link);
+  $('#catFight2').attr('src', images[num2].link);
 };
 
- $.ajax({
-   url: "https://api.imgur.com/3/album/FLB8V/images",
-   headers:{
+$.ajax({
+  url: "https://api.imgur.com/3/album/FLB8V/images",
+  headers:{
     "Authorization": "Client-ID 52a6434c730ea88"
-    }
-  })
- .done(function(res) {
-    console.log(res.data[1]);
-    images = res.data;
-    pageSetup();
+  }
+})
+.done(function(res) {
+  console.log(res.data[1]);
+  $(pageSetup(res.data));
+});
+
+
+$(function() {
+  $('#catFight1').on('click', function(){
+    images.vote += 1;
+    $('#catFight2').attr('src', images[altNum()].imgSrc);
   });
 
-
-
-
-
+  $('#catFight2').on('click', function(){
+    images.vote += 1;
+    $('#catFight1').attr('src', images[altNum()].imgSrc);
+  });
+});
