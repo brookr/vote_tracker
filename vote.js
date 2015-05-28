772400f7cb6899e615f6119002c0b859ee144959
Brook Riggio <brook@rigg.io>
2015-05-28 09:17:20 -0700

    Refactor of random selection logic

--- vote.js
+++ vote.js
@@ -1,30 +1,31 @@
 function Cat(catNum, imgSrc, wins, losses){
+  this.catNum = catNum;
   this.imgSrc = imgSrc;
   this.wins = wins;
   this.losses = losses;
 }
-var catArray = [];
+
+function altNum(num1, num2){
+  var newNum;
+  do {
+   newNum = Math.floor(Math.random() * catArray.length);
+  } while(newNum === num1 || newNum === num2);
+  return newNum
+};
+
+var num1, num2, wins, catArray = [];
 
 for (var i = 1; i <= 14; i++) {
   catArray.push(new Cat([i], 'kittenPics/cat'+[i]+'.jpg', 0, 0))
 };
 
-do {
-  var num1 = Math.floor(Math.random() * catArray.length);
-  var num2 = Math.floor(Math.random() * catArray.length);
-} while(num1 === num2);
-
-function altNum(){
-  do {
-   var newNum = Math.floor(Math.random() * catArray.length);
-   return newNum
-  } while((newNum === num1) && (newNum === num2));
-};
+num1 = Math.floor(Math.random() * catArray.length);
+num2 = altNum(num1, null);
 
 $(function(){
-  $('#catFight1').attr('src', catArray[num1].imgSrc);
-  $('#catFight2').attr('src', catArray[num2].imgSrc);
-  var wins = catArray[num1].wins;
+  $('img#catFight1').attr('src', catArray[num1].imgSrc);
+  $('img#catFight2').attr('src', catArray[num2].imgSrc);
+  wins = catArray[num1].wins;
 
   $('#catFight1').click(function(){
     catArray[num1].wins += 1;
@@ -33,16 +34,14 @@ $(function(){
     console.log(catArray[num1].losses);
     console.log(catArray[num2].wins);
     console.log(catArray[num2].losses);
-    $('#catFight2').attr('src', catArray[altNum()].imgSrc);
+    num2 = altNum(num1, num2);
+    $('#catFight2').attr('src', catArray[num2].imgSrc);
   });
 
   $('#catFight2').click(function(){
-    catArray[num2].wins += 1;
     catArray[num1].losses += 1;
-    $('#catFight1').attr('src', catArray[altNum()].imgSrc);
+    catArray[num2].wins += 1;
+    num1 = altNum(num1, num2);
+    $('#catFight1').attr('src', catArray[num1].imgSrc);
   });
 });
-
-
-
-
